1953f71f64e36343f446ee590fe8cb9a3f8b1c1b
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-09-11 18:52:16 -0300

    Creating a shell script to init Postgres DB through docker-compose file

---
+++ run.sh
@@ -0,0 +1,8 @@
+docker-compose up -d #Start all "services" (containers) defined in docker-compose.yml file in <-d> Daemon mode (running in Background)
+docker-compose ps #List running "services" (containers) mapped in docker-compose.yml file
+
+echo "Waiting Postgres be loaded"
+sleep 5
+
+docker-compose exec db psql -U postgres -c '\l' #Run a command "\l" (list all databases) in "db" "service" (container)
+docker-compose down #Stop all "services" (containers) and remove containers
