d3520297583683f3304c0a44a77654fd6757da0b
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-09-12 13:52:28 -0300

    Adding SQL script to init DB

---
+++ scripts/init.sql
@@ -0,0 +1,22 @@
+/* Creating DB if it doesn't exist */
+select "create database email_sender" where not exists (select from pg_database where datname = "email_sender")\gexec
+
+/* Accessing DB */
+\c email_sender;
+
+/* Creating Table */
+create table emails (
+    id serial not null,
+    data timestamp not null default current_timestamp,
+    subject varchar(100) not null,
+    message varchar(250) not null
+);
+
+/* !!! IMPORTANT !!! The next step is only needed due to DBeaver DB access issues */
+/* See more at https://github.com/dbeaver/dbeaver/issues/3361 */
+/* Creating a User and giving some privileges */
+create user postgres1 with encrypted password 'root';
+grant connect on database email_sender to postgres1;
+grant usage on schema public to postgres1;
+grant all privileges on all tables in schema public to postgres1;
+grant all privileges on all sequences in schema public to postgres1;
