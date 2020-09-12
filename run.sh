49eb5c83bb3dad2facb802e49047271661cbcfa7
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-09-12 14:06:05 -0300

    Connection to DB container and running embedded check.sql script

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ echo "Waiting Postgres be loaded"
 sleep 5
 
 # docker-compose exec db psql -U postgres -c "\l" #Run a command "\l" (list all databases) in "db" "service" (container)
+
+docker-compose exec db psql -U postgres -f /scripts/check.sql #Run the /scripts/check.sql inside the container
