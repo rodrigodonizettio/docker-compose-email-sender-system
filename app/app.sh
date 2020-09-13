bc7af70a1b0df3c7554dde636a45a2b47ed3e496
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-09-13 11:24:45 -0300

    Adding "app.sh" script to start Bottle and execute "sender.py" file
    Moreover,
    - "sender.py" file listens to POST HTTP requests in "/:8080" that will be
    triggered by the "frontend" service.

---
+++ app/app.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pip install bottle==0.12.13 #PIP is a package manager #BOTTLE is a WSGI (Web Server Gateway Interface) simple Web Micro-Framework
+python -u sender.py #Executing "sender.py" file with <-u> that forces STDOUT and STDERR to be unbuffered.
