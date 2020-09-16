869db61b22c028dcfbeaf99ff292294d08c10a81
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-09-15 22:11:56 -0300

    Changing "psycopg2" version due to bug found in version 2.7.1
    Moreover,
    The issue: https://github.com/taigaio/taiga-back/issues/1122

--- app/app.sh
+++ app/app.sh
@@ -3,5 +3,5 @@
 #PIP is a package manager
 #BOTTLE is a WSGI (Web Server Gateway Interface) simple Web Micro-Framework
 #PSYCOPG2 is a Postgres DB adapter
-pip install bottle==0.12.13 psycopg2==2.7.1
+pip install bottle==0.12.13 psycopg2==2.7.3.2
 python -u sender.py #Executing "sender.py" file with <-u> that forces STDOUT and STDERR to be unbuffered.
