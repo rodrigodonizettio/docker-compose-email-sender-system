838abacd678a75aa78252b6b27aae1f707d526d1
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-09-23 19:41:38 -0300

    Importing Redis in "app.sh" for "app" service

--- app/app.sh
+++ app/app.sh
@@ -3,5 +3,6 @@
 #PIP is a package manager
 #BOTTLE is a WSGI (Web Server Gateway Interface) simple Web Micro-Framework
 #PSYCOPG2 is a Postgres DB adapter
-pip install bottle==0.12.13 psycopg2==2.7.3.2
+#REDIS is an in-memory data structure store, used as a Database, Cache and Message Broker 
+pip install bottle==0.12.13 psycopg2==2.7.3.2 redis==2.10.5
 python -u sender.py #Executing "sender.py" file with <-u> that forces STDOUT and STDERR to be unbuffered.
